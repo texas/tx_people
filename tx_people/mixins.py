a38004ce71b804f9b9015d0f5a4e2c7ae8c82911
Travis Swicegood <development@domain51.com>
2013-08-19 18:16:43 -0500

    Add in docblocks for mixins

--- tx_people/mixins.py
+++ tx_people/mixins.py
@@ -4,6 +4,11 @@ from . import fields
 
 
 class ReducedDateStartAndEndMixin(models.Model):
+    """
+    Provides a ``start_date`` and ``end_date`` field for a given model
+
+    This is a common set of fields found throughout Popolo models.
+    """
     class Meta:
         abstract = True
 
@@ -13,7 +18,10 @@ class ReducedDateStartAndEndMixin(models.Model):
 
 class TimeTrackingMixin(models.Model):
     """
-    Mixin for adding a created_at and updated_at field
+    Mixin for adding a ``created_at`` and ``updated_at`` field
+
+    Most entities inside Popolo provide an auto-generated ``created_at``
+    and ``updated_at`` time stamp.
     """
     class Meta:
         abstract = True
