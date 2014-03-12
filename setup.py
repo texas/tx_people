adfcb92cf9a6ed3d91b616aaba559b2077e3a0ed
Ryan Murphy <github@accounts.rdmurphy.org>
2014-03-12 15:56:05 -0500

    Prep branch (1.3.0)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_people'):
 
 setup(
     name='tx_people',
-    version='1.2.0',
+    version='1.3alpha.0',
     description='Texas Tribune: tx_people',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
